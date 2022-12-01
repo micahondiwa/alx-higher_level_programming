095cee060535d001b9b475409af521bce75491de
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-01 13:56:02 +0000

    Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,17 +7,14 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-
     op = sys.argv[2]
     if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-
     from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-
 
     if op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))

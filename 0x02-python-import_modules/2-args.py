f9297973b302ef23130dc09c92727a032e1856f8
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-01 13:53:54 +0000

    Add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,12 @@ if __name__ == "__main__":
     import sys
     i = len(sys.argv) - 1
 
-
     if i == 0:
         print("{} arguments.".format(i))
     elif i == 1:
         print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
-
 
     if i >= 1:
         i = 0

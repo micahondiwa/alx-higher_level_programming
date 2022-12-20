63c1a4bcac138794ea6b615dc8fe4f2774fd88d6
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-20 15:50:36 +0000

    Empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,5 +3,8 @@ class Square:
 
     pass # An empty block
 
+#Creating an object, my_square wich is an insrance of the Square class
 my_square = Square()
+
+#Prints the class and the location in memory.
 print(my_square)

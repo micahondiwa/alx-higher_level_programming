164cd0f589b6a5452712ee0f2dea5931689ab165
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-20 20:36:06 +0000

    Calculating the area of a square object

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/user/bin/python3
+class Square:
+    """
+    A calss Square, defined by the size attribute
+    """" 
+    def __init__(self, size = 0)
+    """
+    Initializing the Object in the Square class
+    """
+        if not isinstance(size, int):
+            raise TypeError("The size of x must be an integer")
+        elif size < 0:
+            raise ValueError("The size of x must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        The area method calculates the area of a Square object
+        """
+        return (self.__size ** 2)

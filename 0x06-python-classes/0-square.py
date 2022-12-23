2bb0fe8d03d09fe1a917c0217ce70d374162c5bc
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-23 08:02:01 +0000

    Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Square:
   pass  # An empty block
-  
-'''Creating an object, my_square wich is an insrance of the Square class'''
+ 
+'''Creating an object my_square wich is an insrance of the Square class'''
 my_square = Square()
 
 '''Prints the class and the location in memory.'''

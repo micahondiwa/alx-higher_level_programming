eb949ca64f1c1e8b42ab81cff9a0bfa59b10c134
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-23 12:55:27 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
 class Square:
+    """Exceptions are documented in the same way as classes.
+    The __init__ method may be documented in either the class level
+    docstring, or as a docstring on the __init__ method itself.
+    Either form is acceptable, but the two should not be mixed. Choose one
+    convention to document the __init__ method and be consistent with it.
+    Note
+    ----
+    Do not include the `self` parameter in the ``Parameters`` section.
+    Parameters
+    ----------
+    msg : str
+        Human readable string describing the exception.
+    code : :obj:`int`, optional
+        Numeric error code.
+    Attributes
+    ----------
+    msg : str
+        Human readable string describing the exception.
+    code : int
+        Numeric error code.
+    """
 
-
-    pass  '''An empty for creating an empty class Square.'''
+    pass

c0e10e8baa5a2a98553c3a8e8265f9939289b2a7
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-23 11:16:51 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 class Square:
-  pass  # An empty block
- 
-'''Creating an object my_square wich is an insrance of the Square class'''
-my_square = Square()
-
-'''Prints the class and the location in memory.'''
-print(type(my_square))
-
-print(my_square)
+    
+    
+    pass  # An empty for creating an empty class Square.

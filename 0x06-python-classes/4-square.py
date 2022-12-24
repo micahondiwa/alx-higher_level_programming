2f733b7726adda2b00f9f0528de8c9b57301a601
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-24 10:41:14 +0000

    Hot fix: indentation, documentation and white spaces

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+'''A class Square, defined by size'''
+
+
 class Square:
-    """
-    A Class called Square, defined by size
-    """
-    def __init__(self, size = 0);
-        """
-        The method that initalizes a sqauer object-an instance of a Square class.
-        """
+    '''A Class called Square, defined by size'''
+    def __init__(self, size=0):
         if  not instance(size, int):
             raise TypeError("The size myst be an integer")
         elif size < 0:
@@ -14,23 +12,18 @@ class Square:
         else:
             self.__size = size
 
+    '''The method returns the area of a square'''
+
     def area(self):
-        """
-        The method returns the area of a square object.
-        """
         return (self.__size ** 2)
 
-    @property
+    '''The method returns the size of a square'''
+   @property
     def size(self):
-        """
-        The method returns the size of a square.
-       """
-
+    
+    '''The method sets the value of a square'''
     @size.setter
     def size(size, value):
-        """
-       The method that sets the value of the size of a Square.
-        """
         if not isinstance(value, int):
             raise TypeError("The size must be an integer")
         elif value < 0:

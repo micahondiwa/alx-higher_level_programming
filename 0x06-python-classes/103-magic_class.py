16a42a825c6788b7996edf76a232cfed55cb5f64
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-24 12:02:19 +0000

    Fix: Indentation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
+"""Defines a magic class"""
+
 import math
 
-class MagicClass:
 
+class MagicClass:
     """Class that stores the properties
      of a circumference"""
+
     def __init__(self, radius = 0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("Radius must be a number")
         self.__radius = radius
 
-    """ Method that calculates the area ofthe circumference"""
     def area(self):
+        """Returns the area of the magicclass"""
         return ((self.__radius ** 2) * math.pi)
 
-    """Method that claculates the perimeter of a circumference"""
     def circumference(self):
+        """Returns the circumference of the magiccalss"""
         return (2 * math.pi * self.__radius)

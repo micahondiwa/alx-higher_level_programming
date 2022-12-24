6728fc0e5454b783ce150c55a77fd4243c5beb38
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-24 10:55:13 +0000

    Hot fix: size setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-'''A class Square, defined by size'''
+'''A module Square, defined by size'''
 
 
 class Square:
     '''A Class called Square, defined by size'''
-    def __init__(self, size=0):
-        if  not instance(size, int):
-            raise TypeError("The size myst be an integer")
-        elif size < 0:
-            raise ValueError("The size must be >= 0")
-        else:
-            self.__size = size
-
-    '''The method returns the area of a square'''
-
-    def area(self):
-        return (self.__size ** 2)
-
-    '''The method returns the size of a square'''
-   @property
-    def size(self):
     
-    '''The method sets the value of a square'''
+    def __init__(self, size=0):
+        '''Initializes square. 
+        Args: size (int); size of a square''''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''The method sets the value of a square
+        Returns: size (int)'''
+        return self.__size
+
     @size.setter
     def size(size, value):
-        if not isinstance(value, int):
+        '''Changes the value of size. 
+        Args: value (int): new value of size'''
+        if type(value) != int:
             raise TypeError("The size must be an integer")
         elif value < 0:
             raise ValueError("The size must be >= 0")
         else:
             self.__size = value
+
+    def area(self):
+        '''Returns the area of a square. 
+        Return: are'''
+        return self.__size ** 2

dfb3e8a2981148657f5dc0ea2b6ac8a8d527cf54
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-24 10:00:27 +0000

    Indentation: Fixed the indetation in the function definition line

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,11 @@ class Square:
     '''Defining a class that defines a square by its size'''
 
 
-def __init__(self, size=0):
-       ''' Method to initialize the square object'''
+    def __init__(self, size=0):
+        ''' Method to initialize the square object'''
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
             self.__size = int(size)

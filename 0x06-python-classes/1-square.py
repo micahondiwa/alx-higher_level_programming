d365495825a591daeeede8e85b7d10f292710629
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-24 09:51:09 +0000

    White space fix: Removed the extra lines with the white spaces

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-'''Defining a class Square'''
-
+'''Defining a class Square with a size attribute'''
 
 
 class Square:
-    """Class Square that defines a square object"""
+    '''Class Square that defines a square object'''
     def __init__(self, size):
         self.__size = size

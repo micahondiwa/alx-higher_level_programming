ae9072a10c4dd631abe4ff521eeadb6f7b7fca5f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-24 09:42:33 +0000

    [Fix white spaces error]: Removed white spaces and fixed identation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+'''Defining a class Square'''
+
+
+
 class Square:
-    """Class Square that defines a square object
-    """
+    """Class Square that defines a square object"""
+
     def __init__(self, size):
         """Initialize method that stores the size of the square
-        Args:
-            param1 (int): size of the square
-        """
+        Args: param1 (int): size of the square"""
         self.__size = size

ac84b52068dfdce33a9bff1fdebeb8b97fe376a8
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-24 09:56:39 +0000

    White space: Fixed the white space and indetation error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """ A class that defines a square by its size
-    """
-    def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
+    '''Defining a class that defines a square by its size'''
+
+
+def __init__(self, size=0):
+       ''' Method to initialize the square object'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:

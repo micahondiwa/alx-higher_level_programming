102867bd2a85771f5710611a6f81296049bebef4
micahondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-24 10:29:48 +0000

    Fix: White spaces and indentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     '''A calss Square, defined by the size attribute'''
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("The size of x must be an integer")
         elif size < 0:

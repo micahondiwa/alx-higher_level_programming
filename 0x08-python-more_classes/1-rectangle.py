a5a47600beae7ddd060e1bfc170888bd4aeb00fb
micahondiwa <micah.okecth.ondiwa@gmail.com>
2023-01-04 16:08:09 +0000

    Fix: Indentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,13 +25,13 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """ method that defines the width
+        ''' method that defines the width
         Args:
         value: width
         Raises:
         TypeError: if width is not an integer
         ValueError: if width is less than zero
-        """
+        '''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -40,21 +40,21 @@ class Rectangle:
 
     @property
     def height(self):
-        """ method that returns the value of the height
+        '''method that returns the value of the height
         Returns:
         height of the rectangle
-        """
+        '''
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ method that defines the height
+        ''' method that defines the height
         Args:
         value: height
         Raises:
         TypeError: if height is not an integer
         ValueError: if height is less than zero
-        """
+        '''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:

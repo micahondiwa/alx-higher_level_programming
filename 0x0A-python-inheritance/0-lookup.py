5eeaf83df4d8955d44f7b181b2bb2f06144d5ad1
micahondiwa <micah.okecth.ondiwa@gmail.com>
2023-01-11 10:28:33 +0000

    Fix: White spaces

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """ Function that returns the list of available attributes
-        and methods of an object
-    Args:
-        obj: instance of the class
-    Returns:
-        List of attributes
-    """
+    """Return a list of an object's available attributes."""
     return (dir(obj))

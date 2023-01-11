d5c5f66005b8dd0be6d7bbc998300d4d9e08efaf
micahondiwa <micah.okecth.ondiwa@gmail.com>
2023-01-11 10:32:18 +0000

    Fix: Added method to the class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+"""
+MyList class module.
+Define MyList class.
+"""
+
+
 class MyList(list):
-    """ Class that inherits the attributes references of class list
-    Args:
-        list: class list
-    """
+    """Define a MyList."""
 
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        """Print the list sorted"""
+        print(sorted(self))

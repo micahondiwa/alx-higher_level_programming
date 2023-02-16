2f7e4c263fc1ec572e1387be3bf2a80fbf95c0c6
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2023-02-16 09:17:43 +0300

    New user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Creates a new user
+-- Creates a new user and grant previleges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';

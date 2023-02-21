fa2cf46b38f607902f7796e155af3f8a5cf74b7f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-02-21 11:19:31 -0800

    Fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
--- Script that creates an user in MySQL server
--- Query to create the user 'user_0d_1' in MySQL server
+-- Script that creates an user i
+-- Query to create the user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES;
-ON * . *;
-TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';

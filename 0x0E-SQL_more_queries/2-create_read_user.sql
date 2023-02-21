d7d6df27ea7f8857927da76e22aacb748fb08fc4
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-02-21 11:17:20 -0800

    Fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,10 @@
---Creates a new user and databases in mySQL server
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+--Creates a new user and 
+-- databases in mySQL server
+CREATE DATABASE IF NOT EXISTS
+hbtn_0d_2;
+CREATE USER IF NOT EXISTS
+'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost'
+= 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* 
+TO 'user_0d_2'@'localhost';

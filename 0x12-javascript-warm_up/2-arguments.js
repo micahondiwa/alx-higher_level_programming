640989f97ec195883c4dccf893f0c4b730262d32
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-13 06:46:59 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length < 1) {
 	console.log('No argument');
-}
-else if (process.argv.length === 1) {
+} else if (process.argv.length === 1) {
 	console.log('Argument found');
-}
-else {
+} else {
 	console.log('Arguments found');
 }

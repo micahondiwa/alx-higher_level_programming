6c98518a89b2ea2edda65d783b004f5ce77b3a71
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-15 04:55:10 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,19 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
+const dict = require('./101-data.js').dict;
 const newDict = {};
-
-Object.keys(dict).map(function (key, index) {
+for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [];
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
-  newDict[dict[key]].push(key);
-});
-
+}
 console.log(newDict);
+/* exports.dict = {
+  89: 1,
+  90: 2,
+  91: 1,
+  92: 3,
+  93: 1,
+  94: 2
+}; */

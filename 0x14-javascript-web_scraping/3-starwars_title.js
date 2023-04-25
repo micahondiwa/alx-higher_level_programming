70c9599fb5790768e98bbb5c53eea38121a8511e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-04-25 12:00:07 -0700

    Starwares

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
+request('https://swapi-api.alx-tools.com/api/films/' + id + '/', function (error, response, body) {
   if (error == null) {
     const json = JSON.parse(body);
     console.log(json.title);
